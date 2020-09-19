942986a14a983c744939e878bbb7e3f4f536aa8f
oasamayoa <oasamayoa14@gmail.com>
2020-09-19 14:53:28 -0600

    implementacion de plantila adminlta y creacion de login

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('',include(('bases.urls', 'bases'), namespace='bases') ),
+    # path('registro/',include(('registro.urls', 'registro'), namespace='registro') ),
+    # path('che/',include(('che.urls', 'che'), namespace='che') ),
     path('admin/', admin.site.urls),
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
